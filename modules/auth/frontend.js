d77d6822feef8f138588b139aa8985ac876b9dd9
Michael Matveev <xtreme@rh1.ru>
2016-05-06 23:23:02 +0300

    Session-related bug fixed

--- modules/auth/frontend.js
+++ modules/auth/frontend.js
@@ -29,7 +29,10 @@ module.exports = function(app) {
         logout = function(req, res, next) {
             if (req.session && req.session.auth_id) {
                 delete req.session.auth_id;
-                return res.redirect(303, "/?logout=" + Math.random().toString().replace('.', ''));
+				delete req.session.auth_data;
+				req.session.save(function() {
+					return res.redirect(303, "/?logout=" + Math.random().toString().replace('.', ''));
+				});                
             } else {
                 return res.redirect(303, "/auth/login?rnd=" + Math.random().toString().replace('.', ''));
             }
